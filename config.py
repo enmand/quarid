f178e7a8e6fadd3e4c4914ce40dbfc7553c302f7
Daniel Enman <enmand@unerror.com>
2012-03-05 00:15:39 -0400

    Use shlex for configuration, and pass config file by default

--- config.py
+++ config.py
@@ -1,15 +1,60 @@
-import configparser
+import shlex
 
 class Config:
-	def __init__(self):
-		self.cfg_file = configparser.ConfigParser()
-		self.cfg_file.read('bot.cfg')
+	__lex = None;
+	__cfg = {}
 
-	def dict(self):
-		cfg = dict()
-		for s in self.cfg_file.sections():
-			cfg[s] = dict()
-			for o in self.cfg_file.options(s):
-				cfg[s][o] = self.cfg_file.get(s, o)
+	def __init__(self, filename = "bot.cfg"):
+		__file = open(filename, 'r');
+		self.__lex = shlex.shlex(__file);
+		self.__read()
 
-		return cfg
+	def __read(self):
+		tok = self.__lex.get_token();
+		prevtok = None;
+
+		base = None;
+		opt = None;
+		arr = None;
+		while tok != self.__lex.eof:
+			if tok == ".":
+				base = prevtok;
+				if base not in self.__cfg:
+					self.__cfg[base] = {};
+			elif tok == "True" or tok == "False":
+				tok = bool(tok);
+			elif tok.isdigit():
+				try:
+					tok = int(tok);
+				except ValueError:
+					tok = float(tok);
+			elif tok[0] == '"' and tok[-1] == '"':
+				tok = tok[1:-1];
+			elif tok == "]":
+				self.__cfg[base][opt] = arr;
+				arr = None;
+
+			if prevtok == ".":
+				opt = tok;
+			elif prevtok == "=":
+				self.__cfg[base][opt] = tok;
+			elif prevtok == "[":
+				arr = [];
+
+			if arr is not None:
+				if prevtok is not "[":
+					arr.append(tok);
+
+
+
+			prevtok = tok;
+			tok = self.__lex.get_token();
+
+	def get(self, group = None):
+		if group is not None:
+			return self.__cfg[group]
+		return self.__cfg;
+
+if __name__ == "__main__":
+	cfg = Config('bot.cfg');
+	print(cfg.dict());
--- quarid.py
+++ quarid.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import irc
 import config
+import module
+
 import os
 import sys
 import signal
@@ -12,14 +14,17 @@ def main():
 	parser = argparse.ArgumentParser(description = "qpirc (Quard Python IRC) bot: version %s" % irc.IRC.version(),
 									 epilog="Visit http://github.com/enmand/qpirc for more information");
 	parser.add_argument("--version", action="version", version="%s" % irc.IRC.version())
+	parser.add_argument("--config", "-c", default="bot.cfg", help="Configuration file to use (default bot.cfg");
 	parser.add_argument("action", choices=["start", "stop", "reload"]);
 	args = parser.parse_args()
+
 	if args.action == "start":
-		start();
+		start(args.config);
 	elif args.action == "stop":
 		stop();
 	elif args.action == "reload":
 		print ("Not yet implemented")
+
 
 """
 Background a process of the bot, so we don't have a controlling tty,
@@ -30,7 +35,7 @@ def deamon():
 		if os.fork() > 0:
 			sys.exit(0) #close the parent
 	except OSError as e:
-		sys.stderr.write("Failed for background process... exiting. (got %s)" % e.strerr);
+		sys.stderr.write("Failed for background process... exiting. (got %s)\n" % e.strerr);
 		sys.exit(1)
 
 	os.chdir('.')
@@ -41,7 +46,7 @@ def deamon():
 		if os.fork() > 0: # avoid zombie processes
 			sys.exit(0); # close the second child
 	except OSError as e:
-		sys.stderr.write("Failed for background process... exiting. (got %s)" % e.strerr);
+		sys.stderr.write("Failed for background process... exiting. (got %s)\n" % e.strerr);
 		sys.exit(1);
 
 	atexit.register(rmpid)
@@ -52,7 +57,7 @@ def deamon():
 """
 Start the bot, and write the pid
 """
-def start():
+def start(cfg_file):
 	# Do we have a pid (i.e. is the bot running?)
 	try:
 		fd = open('qpirc.pid', 'r');
@@ -62,11 +67,11 @@ def start():
 		pid = None;
 
 	if pid:
-		sys.stderr.write("We're already running as %s" % pid);
+		sys.stderr.write("We're already running as %s\n" % pid);
 		sys.exit(1);
 
 	deamon();
-	qpirc();
+	qpirc(cfg_file);
 
 """
 Close the bot
@@ -80,32 +85,30 @@ def stop():
 		pid = None;
 
 	if not pid:
-		sys.stderr.write("No pidfile found -- are you sure it's running?");
+		sys.stderr.write("No pidfile found -- are you sure it's running?\n");
 		rmpid();
-		os.exit(0);
+		sys.exit(0);
 
 	try:
 		os.kill(pid, signal.SIGTERM);
 	except OSError:
-		sys.stderr.write("Couldn't terminate the process -- is it running?")
+		sys.stderr.write("Couldn't terminate the process -- is it running?\n")
 		rmpid();
 	rmpid();
 
 """
 Start the bot, register it's modules and run the main loop
 """
-def qpirc():
-	conf = config.Config().dict()
+def qpirc(cfg_file):
+	conf = config.Config(cfg_file)
 	bot = irc.IRC.factory()
-
-	## HERE IS WHERE WE WANT TO ADD SOME MODULES
 
 	bot.addListener('376', printAndJoin);
 	bot.addListener('privmsg', do_something)
 
-
-	core = conf['core']
-	bot.connect(core['host'], core['port'], core['nick'], core['pass'], conf['ssl']['use_ssl'])
+	core = conf.get('irc');
+	
+	bot.connect(core['host'], core['port'], core['nick'], core['pass'], conf.get('ssl')['use'])
 	bot.run();
 
 """
